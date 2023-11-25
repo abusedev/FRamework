93d430be8367bc4a229c6621677f3ad52c7abc7b
romance <66000596+romancewastaken@users.noreply.github.com>
2023-11-24 21:16:07 -0500

    Add files via upload

---
+++ handlers/stringEncoding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace FRamework.handlers
+{
+    public class stringEncoding
+    {
+        public static string encode(string input)
+        {
+            var text = Encoding.UTF8.GetBytes(input);
+            return Convert.ToBase64String(text);
+        }
+
+        public static string decode(string input) 
+        {
+            var encoded = Convert.FromBase64String(input);
+            return Encoding.UTF8.GetString(encoded);
+        } 
+    }
+}
