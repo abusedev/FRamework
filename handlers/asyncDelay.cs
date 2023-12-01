eb2d2dbb56f13d5debd04402fd508d6914223086
romance <66000596+romancewastaken@users.noreply.github.com>
2023-11-30 23:57:46 -0500

    Add files via upload

---
+++ handlers/asyncDelay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FRamework.handlers
+{
+    internal class asyncDelay
+    {
+        /// <summary>
+        /// Delay using async, a method can be added to run after delaying
+        /// </summary>
+        /// <param name="ms">Time to wait</param>
+        /// <param name="function">Method to call after delay, can be set to null to not run a function</param>
+        /// <param name="functionTask">If set to true, the function will be ran as a Task, if set to false it will run as normal</param>
+        public static async void delay(int ms, Action function, bool functionTask)
+        {
+            await Task.Delay(ms);
+            if (function != null)
+            {
+                if (functionTask)
+                {
+                    threads.createTask(function);
+                }
+                else
+                {
+                    function();
+                }
+            }
+        }
+    }
+}
