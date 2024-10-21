b004cdf944466e38401373448e1df78b53021409
romance <romance@DESKTOP-U3R1PND>
2024-10-21 19:35:29 -0400

    add serviceManager

---
+++ handlers/windows/serviceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceProcess;
+
+namespace FRamework.handlers.windows
+{
+    internal class serviceManager
+    {
+        public static void stopService(string serviceName)
+        {
+            ServiceController serviceController = new ServiceController(serviceName);
+            if ((serviceController.Status.Equals(ServiceControllerStatus.Running)) || (serviceController.Status.Equals(ServiceControllerStatus.StartPending)) || (serviceController.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                serviceController.Stop();
+            }
+        }
+
+        public static void restartService(string serviceName)
+        {
+            ServiceController serviceController = new ServiceController(serviceName);
+            if ((serviceController.Status.Equals(ServiceControllerStatus.Running)) || (serviceController.Status.Equals(ServiceControllerStatus.StartPending)) || (serviceController.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                serviceController.Stop();
+            }
+            serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+            serviceController.Start();
+            serviceController.WaitForStatus(ServiceControllerStatus.Running);
+        }
+    }
+}
